fdaf6109d12422b0e443e60c24bf5e5a24f2b3df
dev-knz <knzfpsoficial@gmail.com>
2026-01-28 14:03:14 -0300

    Estudos do dia: cores
    - Estudo teórico das cores ANSI

---
+++ basico/cores.py
@@ -0,0 +1,21 @@
+# ANSI - contra barra 
+# \033[style; text; back m
+
+# Style
+
+# 0 - None
+# 1 - Bold (Negrito)
+# 4 - Underline (Sublinhado)
+# 7 - Negative 
+
+# Text
+
+# 30 - 37 
+# Branco[30], Vermelho[31], Verde[32], Yellow[33], Azul[34], Roxo[35], Ciano[36] e Cinza[37]
+
+# Background
+
+# 40 - 47
+# Branco[40], Vermelho[41], Verde[42], Yellow[43], Azul[44], Roxo[45], Ciano[46] e Cinza[47]
+
+print('\033[0;35;34mOlá, Mundo!\033[m')
