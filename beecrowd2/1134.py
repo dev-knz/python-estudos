a2114e17b60c08e4cd0555f0db211b6538a74aa5
dev-knz <knzfpsoficial@gmail.com>
2026-05-30 22:28:30 -0400

    Atividades em Python

--- beecrowd2/1134.py
+++ beecrowd2/1134.py
@@ -5,3 +5,19 @@ Escreva a sua solução aqui
 Code your solution here
 Escriba su solución aquí
 '''
+
+
+n = int(input())
+
+entrada = input().split()
+matriz = []
+
+for i in range(n):
+    matriz.append(int(entrada[i]))
+
+fator = int(input())
+quantidade = 0
+
+for i in range(n):
+    if matriz[i] == fator:
+        quantidade += 1
--- beecrowd2/1135.py
+++ beecrowd2/1135.py
@@ -1,7 +1,14 @@
-# -*- coding: utf-8 -*-
-
 '''
 Escreva a sua solução aqui
 Code your solution here
 Escriba su solución aquí
 '''
+
+n = int(input())
+
+entrada = input().split()
+matriz = []
+
+for i in range(n):
+    matriz.append(int(entrada[i]))
+
