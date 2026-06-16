bac64419bc71c67bcf376d03c646ed8b281e5b00
dev-knz <knzfpsoficial@gmail.com>
2026-06-15 22:15:04 -0400

    Estudos python

--- beecrowd2/1153.py
+++ beecrowd2/1153.py
@@ -1,7 +1,18 @@
-# -*- coding: utf-8 -*-
+n1, n2 = map(int, input().split())
 
-'''
-Escreva a sua solução aqui
-Code your solution here
-Escriba su solución aquí
-'''
+l1 = list(map(int, input().split()))
+l2 = list(map(int, input().split()))
+
+l3 = []
+for i in range(n2):
+    for j in range(n1):
+        if l2[i] == l1[j]:
+            l3.append(l2[i])
+if l3 == l2:
+    print('S')
+else:
+    print('N')
+        
+    
+              
+
--- beecrowd2/1154.py
+++ beecrowd2/1154.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
+s, t = map(int, input().split())
 
-'''
-Escreva a sua solução aqui
-Code your solution here
-Escriba su solución aquí
-'''
+lista = []
+for i in range(t):
+    lista.append(list(map(int, input().split())))
+
+n = int(input())
+
+caminhos = []
+contagem = 0
+
+for i in range(n):
+    caminhos.append(list(map(int, input().split())))
+    
+    verdade = True
+    for j in range(len(caminhos[i])):
+        if j + 1 < len(caminhos) - 1:
+            if not (caminhos[i][j] and caminhos[i][j+1]) in lista:
+                verdade = False
+    
+    if verdade:
+        contagem += 1
+
+print(contagem)
