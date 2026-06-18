5eaf9ad0f883ea04f31df5205fe2269f976bbed7
dev-knz <knzfpsoficial@gmail.com>
2026-06-17 22:54:45 -0400

    Estudos Python

--- beecrowd2/1168.py
+++ beecrowd2/1168.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
+def Le_Lista(num):
+    lista = []
 
-'''
-Escreva a sua solução aqui
-Code your solution here
-Escriba su solución aquí
-'''
+    for i in range(num):
+        lista.append(int(input()))
+    return lista
+
+def Inverte_Lista(lista):
+    inicio, fim = 0, len(lista) - 1
+    while inicio < fim:
+        lista[inicio], lista[fim] = lista[fim], lista[inicio]
+        inicio += 1
+        fim -= 1
+    return lista
+
+def Imprime_Lista(lista):
+    for i in range(len(lista)):
+        print(lista[i], end=' ')
+
+n = int(input())
+lista = Le_Lista(n)
+lista = Inverte_Lista(lista)
+Imprime_Lista(lista)
+
--- beecrowd2/1169.py
+++ beecrowd2/1169.py
@@ -1,7 +1,25 @@
-# -*- coding: utf-8 -*-
+def Le_Lista():
+    lista = []
 
-'''
-Escreva a sua solução aqui
-Code your solution here
-Escriba su solución aquí
-'''
+    entrada = input().split()
+    for i in range(len(entrada)):
+        lista.append(int(entrada[i]))
+    return lista
+
+def Remove_Repetidos(lista):
+    resposta = []
+    
+    for i in range(len(lista)):
+        repetido = False
+        for k in range(i):
+            if lista[i] == lista[k]:
+                repetido = True
+        
+        if not repetido:
+            resposta.append(lista[i])
+    return resposta
+
+lista = Le_Lista()
+lista = Remove_Repetidos(lista)
+
+print(lista)
