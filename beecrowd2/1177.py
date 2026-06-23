e133e9904331b6c1f1b4fb7ab5abb57696ab3099
dev-knz <knzfpsoficial@gmail.com>
2026-06-22 22:56:30 -0400

    Estudos de apostila

--- beecrowd2/1177.py
+++ beecrowd2/1177.py
@@ -7,9 +7,12 @@ def segundo_grau():
     n2 = int(input())
     n3 = int(input())
 
-    delta = n2**2 - (4 * n1 * n3)
-    x1 = ((n2*-1) + sqrt(delta)) / 2 * n1
-    x2 = ((n2*-1) - sqrt(delta)) / 2 * n1
+    try:
+        delta = n2**2 - (4 * n1 * n3)
+        x1 = ((n2*-1) + sqrt(delta)) / 2 * n1
+        x2 = ((n2*-1) - sqrt(delta)) / 2 * n1
+    except:
+        return 'Raizes Não Reais'
     return x1, x2
 
 print(segundo_grau())
--- beecrowd2/1178.py
+++ beecrowd2/1178.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
+"""1) Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B.
+A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o
+valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados
+nas variáveis"""
 
-'''
-Escreva a sua solução aqui
-Code your solution here
-Escriba su solución aquí
-'''
+a = 20
+b = 10
+
+a, b = b, a
+print(a, b)
+
+# Ou
+
+a = 20
+b = 10
+
+aux = a
+a = b
+b = aux
+
+print(a, b)
